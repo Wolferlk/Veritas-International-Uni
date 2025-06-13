fe83cd602df857dd48e2b32327b3b08f87f3ac6b
IT22025180 <it22025180@my.sliit.lk>
2025-06-13 14:04:02 +0530

    Our team

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -10,6 +10,7 @@ import { StudentTestimonials } from "./pages/StudentTestimonials/StudentTestimon
 import { CourseDetails } from "./pages/CourseDetails/CourseDetails";
 import CampusLife from "./pages/CampusLife/CampusLife";
 import Partners from "./pages/Partners/Partners";
+import OurTeam from "./pages/Team/OurTeam";
 
 export const App = (): JSX.Element => {
   return (
@@ -24,6 +25,7 @@ export const App = (): JSX.Element => {
         <Route path="/course/:id" element={<CourseDetails />} />
         <Route path="/campus-life" element={<CampusLife />} />
         <Route path="/partners" element={<Partners />} />
+        <Route path="/team" element={<OurTeam />} />
       </Routes>
     </Layout>
   );
